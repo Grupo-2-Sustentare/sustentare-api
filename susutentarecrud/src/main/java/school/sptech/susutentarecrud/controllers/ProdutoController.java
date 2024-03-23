6d00f01762ddf5cbb20901229b11595ee5d9cd3b
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2024-03-23 12:19:27 -0300

    V2 Api java, agora com ordenacao

--- susutentarecrud/src/main/java/school/sptech/susutentarecrud/controllers/ProdutoController.java
+++ susutentarecrud/src/main/java/school/sptech/susutentarecrud/controllers/ProdutoController.java
@@ -10,6 +10,8 @@ import school.sptech.susutentarecrud.domain.produto.ProdutoDTO;
 import school.sptech.susutentarecrud.domain.produto.ProdutoRespository;
 import school.sptech.susutentarecrud.mappers.ProdutoMapper;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +22,26 @@ public class ProdutoController {
 
     @GetMapping
     public ResponseEntity getAllProduct(){
-        var allProducts = produtoRepository.findAll();
+        List<Produto> allProducts = produtoRepository.findAll();
+        selectionSort2(allProducts);
         return ResponseEntity.ok(allProducts);
     }
+
+    @GetMapping("/selectionSort")
+    public ResponseEntity todosProdutosOrdenadoID(){
+        List<Produto> allProducts = produtoRepository.findAll();
+        selectionSort(allProducts);
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @GetMapping("/bubbleSort")
+    public ResponseEntity todosProdutosOrdenadoPreco(){
+        List<Produto> allProducts = produtoRepository.findAll();
+        bubbleSort(allProducts);
+        return ResponseEntity.ok(allProducts);
+    }
+
+
     @PostMapping
     public ResponseEntity<Produto> cadastrarProduto(@RequestBody ProdutoDTO dados){
         Produto produto = ProdutoMapper.INSTANCE.toProduto(dados);
@@ -65,6 +84,57 @@ public class ProdutoController {
         }
         return ResponseEntity.status(400).build();
     }
+
+
+
+
+
+
+    public static List<Produto> selectionSort(List<Produto> v){
+        for (int i = 0; i < v.size() - 1; i++) {
+            for (int j = i + 1 ; j < v.size(); j++) {
+                if ( v.get(j).getId() < v.get(i).getId()){
+                    Produto aux = v.get(j);
+                    v.set(j,v.get(i));
+                    v.set(i,aux);
+                }
+
+            }
+        }
+        return v;
+    }
+
+
+    public static List<Produto> selectionSort2(List<Produto> v){
+
+        for (int i = 0; i < v.size() - 1; i++) {
+            int menorIndice = i;
+            for (int j = i + 1 ; j < v.size(); j++) {
+                if (v.get(j).getEmpresa().compareTo(v.get(menorIndice).getEmpresa()) < 0){
+                    menorIndice = j;
+                }
+            }
+            Produto aux = v.get(i);
+            v.set(i, v.get(menorIndice));
+            v.set(menorIndice, aux);
+        }
+        return v;
+    }
+
+
+    public static List<Produto> bubbleSort(List<Produto> v) {
+        for (int i = 0; i < v.size() - 1; i++) {
+            for (int j = 1; j < v.size() - i; j++) {
+                if (v.get(j-1).getPreco() < v.get(j).getPreco()) {
+                    Produto aux = v.get(j);
+                    v.set(j, v.get(j - 1));
+                    v.set(j - 1, aux);
+                }
+            }
+        }
+        return v;
+    }
+
 
 
 
