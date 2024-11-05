cd11a8b079e6038ee87ae46a5b1b78191afe8ac3
PereiraGus <gustavo.oito@hotmail.com>
2024-11-04 21:40:00 -0300

    Correção dos dados de Compras Regulares vs Não Planejadas.

--- sustentaree/src/main/java/com/example/sustentaree/dtos/ComprasDTO.java
+++ sustentaree/src/main/java/com/example/sustentaree/dtos/ComprasDTO.java
@@ -1,24 +1,14 @@
 package com.example.sustentaree.dtos;
 
 public class ComprasDTO {
-  private String mesAno;
   private String tipoCompra;
   private Integer qtdCompras;
 
-  public ComprasDTO(String mesAno, String tipoCompra, Integer qtdCompras) {
-    this.mesAno = mesAno;
+  public ComprasDTO(String tipoCompra, Integer qtdCompras) {
     this.tipoCompra = tipoCompra;
     this.qtdCompras = qtdCompras;
   }
 
-  // Getters e Setters
-  public String getMesAno() {
-    return mesAno;
-  }
-
-  public void setMesAno(String mesAno) {
-    this.mesAno = mesAno;
-  }
 
   public String getTipoCompra() {
     return tipoCompra;
