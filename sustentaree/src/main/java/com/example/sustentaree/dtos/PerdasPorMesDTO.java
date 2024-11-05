2b7e02911eb2dd2bfb3bd34b704be48765411ae2
PereiraGus <gustavo.oito@hotmail.com>
2024-11-04 21:39:47 -0300

    Correção dos dados de Perdas por mês.

--- sustentaree/src/main/java/com/example/sustentaree/dtos/PerdasPorMesDTO.java
+++ sustentaree/src/main/java/com/example/sustentaree/dtos/PerdasPorMesDTO.java
@@ -1,22 +1,12 @@
 package com.example.sustentaree.dtos;
 
 public class PerdasPorMesDTO {
-  private String mesAno;
   private String tipoPerda;
   private Integer qtdPerda;
 
-  public PerdasPorMesDTO(String mesAno, String tipoPerda, Integer qtdPerda) {
-    this.mesAno = mesAno;
+  public PerdasPorMesDTO(String tipoPerda, Integer qtdPerda) {
     this.tipoPerda = tipoPerda;
     this.qtdPerda = qtdPerda;
-  }
-
-  public String getMesAno() {
-    return mesAno;
-  }
-
-  public void setMesAno(String mesAno) {
-    this.mesAno = mesAno;
   }
 
   public String getTipoPerda() {
